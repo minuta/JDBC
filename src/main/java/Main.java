d79a68a758fe0ab17eda7f34e81f041676452e06
AF <gammamustang@gmail.com>
2020-03-03 19:25:19 +0100

    refactor MySqlConnector and add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        final String DB_NAME = "acme";
+        final String DB_HOST = "jdbc:mysql://localhost:3306/";
+        final String USER_NAME = "qp";
+        final String PASSWORD = "12345";
+
+
+        MySqlConnector mySqlConnector = new MySqlConnector(DB_HOST, DB_NAME, USER_NAME, PASSWORD);
+        String sqlCommand = "select * from users";
+
+        mySqlConnector.runQuery(sqlCommand);
+    }
+}
--- src/main/java/MySqlConnector.java
+++ src/main/java/MySqlConnector.java
@@ -2,21 +2,23 @@ import java.sql.*;
 
 public class MySqlConnector {
 
-    public static final String DB_NAME = "acme";
-    public static final String DB_HOST = "jdbc:mysql://localhost:3306/";
-    public static final String USER_NAME = "qp";
-    public static final String PASSWORD = "12345";
+    private String dbName;
+    private String dbHost;
+    private String userName;
+    private String userPass;
 
-    public static final String DB_URL = DB_HOST + DB_NAME;
+    public MySqlConnector(String dbHost, String dbName, String userName, String userPass) {
+        this.dbName = dbName;
+        this.dbHost = dbHost;
+        this.userName = userName;
+        this.userPass = userPass;
+    }
 
-    public static void main(String[] args){
+    public void runQuery (String sqlCommand){
 
-        try(Connection connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD)) {
+        try(Connection connection = DriverManager.getConnection(dbHost + dbName, userName, userPass)) {
             try(Statement statement = connection.createStatement()){
-
-                String sql = "select * from users";
-
-                try(ResultSet result = statement.executeQuery(sql)){
+                try(ResultSet result = statement.executeQuery(sqlCommand)){
                     while(result.next()) {
                         int id = result.getInt(1);
                         String firstName = result.getString("first_name");
@@ -28,6 +30,37 @@ public class MySqlConnector {
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    public String getDbName() {
+        return dbName;
+    }
+
+    public String getDbHost() {
+        return dbHost;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPass() {
+        return userPass;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    public void setDbHost(String dbHost) {
+        this.dbHost = dbHost;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPass(String userPass) {
+        this.userPass = userPass;
     }
 }
